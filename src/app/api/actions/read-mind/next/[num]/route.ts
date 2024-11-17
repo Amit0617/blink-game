9b5a96634a723d74a9d93b7f8a32835eec8c0338
Amit0617 <71893015+Amit0617@users.noreply.github.com>
2024-11-17 20:44:22 +0530

    Finalizing waves matching and DB calls

--- src/app/api/actions/read-mind/next/[num]/route.ts
+++ src/app/api/actions/read-mind/next/[num]/route.ts
@@ -1,6 +1,26 @@
-import { Action, createActionHeaders } from '@solana/actions';
-
+import { Action, CompletedAction, createActionHeaders } from '@solana/actions';
+import JSDOM from 'jsdom';
 const headers = createActionHeaders();
+
+function splitNumber(num: number) {
+  // Ensure the input is a 4-digit number
+  if (num >= 1000 && num <= 9999) {
+    const firstThreeDigits = Math.floor(num / 10); // Remove the last digit
+    const lastDigit = num % 10; // Get the last digit
+
+    return [firstThreeDigits, lastDigit];
+  } else {
+    return 'Input must be a 4-digit number';
+  }
+}
+
+let response: {
+  title: any;
+  extract: any;
+  description: any;
+};
+
+const randomNumber = Math.floor(Math.random() * 1000);
 
 export const POST = async (req: Request) => {
   try {
@@ -12,12 +32,12 @@ export const POST = async (req: Request) => {
     ).toString();
 
     // generate a random number between 0 and 999
-    const randomNumber = Math.floor(Math.random() * 1000);
+    const exampleNumber = 1234;
 
     if (num == '1') {
       let payload: Action = {
         type: 'action',
-        icon: 'http://127.0.0.1:3000/homepage.png',
+        icon: `${requestUrl.origin}/homepage.png`,
         title:
           'Step 1: Reverse the Digits of Your Number and Subtract the Smaller from the Larger',
         description:
@@ -38,7 +58,7 @@ export const POST = async (req: Request) => {
       console.log('Step 2');
       let payload: Action = {
         type: 'action',
-        icon: 'http://127.0.0.1:3000/homepage.png',
+        icon: `${requestUrl.origin}/homepage.png`,
         title:
           'Step 2: Use the result and Reverse the Digits of Your Number. Then Add both numbers together.',
         description: 'e.g. If you got 456, the result is 456 + 654 = 1110.',
@@ -55,10 +75,10 @@ export const POST = async (req: Request) => {
       };
       return Response.json(payload, { headers });
     } else if (num === '3') {
-      console.log('Step 3');
+      console.log('Step 3', randomNumber);
       let payload: Action = {
         type: 'action',
-        icon: 'http://127.0.0.1:3000/homepage.png',
+        icon: `${requestUrl.origin}/homepage.png`,
         title: `Step 3: Add Random Number to the Result: ${randomNumber}`,
         description: `e.g. If you got 1110, the result is 1110 + ${randomNumber} = ${
           1110 + randomNumber
@@ -76,6 +96,82 @@ export const POST = async (req: Request) => {
       };
       return Response.json(payload, { headers });
     } else if (num === '4') {
+      let payload: Action = {
+        type: 'action',
+        icon: `${requestUrl.origin}/homepage.png`,
+        title:
+          'Step 4: You must have a 4 digit number now! Enter the first 3 digits in Wikipedia index(https://en.wikipedia.org/wiki/Special:AllPages/{FirstThreeDigits}) and open the last digit article.',
+        description: `e.g. If you got ${exampleNumber}, navigate to https://en.wikipedia.org/wiki/Special:AllPages/123 and open the 4th article.\n Make sure to keep it private! Open that link in incognito mode or maybe on another device. Open the article and think about the title and read a bit about it. We will try to guess the article.`,
+        label: 'Next',
+        links: {
+          actions: [
+            {
+              type: 'post',
+              label: 'Next',
+              href: `${baseHref}/4`,
+            },
+          ],
+        },
+      };
+      return Response.json(payload, { headers });
+    } else if (num === '5') {
+      let payload: Action = {
+        type: 'action',
+        icon: `${requestUrl.origin}/homepage.png`,
+        title: 'Step 5: Think of the Article and Click Next',
+        description:
+          'Think of the article title, specifically what is it, take a moment to craft a one liner in your mind to explain it to someone else. We are trying to match your brain waves!',
+        label: 'Next',
+        links: {
+          actions: [
+            {
+              type: 'post',
+              label: 'Next',
+              href: `${baseHref}/5`,
+            },
+          ],
+        },
+      };
+      return Response.json(payload, { headers });
+    } else if (num === '6') {
+      let numbers = splitNumber(1089 + randomNumber);
+      console.log(numbers, randomNumber);
+      if (numbers === 'Input must be a 4-digit number') {
+        return new Response('Retry, There was some error!', {
+          status: 500,
+          headers,
+        });
+      }
+      let el: any = new JSDOM.JSDOM().window.document.createElement('div');
+      await fetch(
+        `${process.env.SCRAPE_URL}${process.env.SCRAPE_API}&url=${process.env.WIKIPEDIA}${numbers[0]}`,
+      )
+        .then((response) => {
+          return response.json();
+        })
+        .then((data) => {
+          el.innerHTML = data;
+        });
+      let similarBrainWavesList =
+        el.getElementsByClassName('mw-allpages-chunk')[0];
+      let desiredArticle = similarBrainWavesList
+        .getElementsByTagName('li')
+        [numbers[1] - 1].getElementsByTagName('a')[0].title;
+      await fetch(`${process.env.WIKI_DB}${desiredArticle}`)
+        .then((response) => {
+          return response.json();
+        })
+        .then((data) => {
+          response = data;
+        });
+      let payload: CompletedAction = {
+        type: 'completed',
+        icon: `${requestUrl.origin}/homepage.png`,
+        title: `Are you thinking of a ${response.description} ?`,
+        description: `Is this article about ${response.title}? We found a great match: ${response.extract}`,
+        label: 'Finish',
+      };
+      return Response.json(payload, { headers });
     }
   } catch (err) {
     console.log(err);
